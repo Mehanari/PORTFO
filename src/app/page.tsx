4c8e1503d32dcd26fff71855772d39063a0dfe01
Anastasiia <nastialime569@gmail.com>
2024-05-17 20:21:22 +0200

    Created a main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,209 @@
-import Image from "next/image";
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import EmblaCarousel from './components/EmblaCarousel';
+import { OPTIONS, SLIDES } from './index';
+
+function sendToSignUp(router: ReturnType<typeof useRouter>) {
+  router.push('/signUp');
+}
+
+//TO DO pushing to the signIn page
+
+// function sendToSignIn(router: ReturnType<typeof useRouter>) {
+//   router.push('/signIn');
+// }
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col bg-orange-200 justify-center min-h-screen">
+      <header>
+        <nav className="text-gray-400 p-4 shadow-md fixed w-full top-0 bg-white z-50">
+          <div className="max-w-7xl mx-auto flex justify-between items-center pr-6">
+            <div className="flex items-center">
+              <Image src="/LogoCut.png" alt="Logo" width={160} height={60} />
+            </div>
+            <div className="flex space-x-4">
+              <button
+                onClick={() => sendToSignUp(router)}
+                className="flex items-center font-semibold"
+              >
+                Sign Up
+              </button>
+              <button
+                // onClick={() => sendToSignIn(router)}
+                className="flex items-center font-semibold"
+              >
+                Sign In
+              </button>
+              <button className="bg-orange-400 hover:bg-orange-300 text-white font-semibold py-2 px-6 rounded-full flex items-center shadow-md">
+                Create Portfolio
+              </button>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <section className="flex flex-col lg:flex-row shadow-md pt-28 pb-10" style={{ zIndex: 10 }}>
+        <div className="lg:w-1/3 flex justify-center items-center pl-14 pr-10">
+          <div>
+            <h1 className="text-black font-bold text-4xl mb-4 lg:text-4xl">
+              SHOW YOUR SKILLS WITH ONLINE PORTFOLIO
+            </h1>
+            <p className="text-gray-700 lg:text-base">
+              Discover the benefits of creating, customizing, and enhancing an online portfolio for work opportunities.
+            </p>
+            <button className="bg-black hover:bg-gray-800 text-white font-semibold py-2 px-6 rounded-full flex items-center shadow-md mt-4">
+              Create Portfolio
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+        <div className="lg:w-2/3 pr-14 pl-14">
+          <Image src="/LaptopFirst.png" alt="Laptop" width={2000} height={1000} layout="responsive"/>
+        </div>
+      </section>
+      <section className="flex flex-col lg:flex-row bg-white pt-20 justify-center items-center">
+        <div className="flex flex-col lg:w-1/2 pr-14 pl-14">
+          <h1 className="text-black font-bold text-4xl mb-4 lg:text-6xl">EXPLORE OUR TEMPLATES</h1>
+        </div>
+        <div className="flex flex-col lg:w-1/3 pr-14 pl-14">
+          <h1 className="text-black font-medium text-2xl mb-4">
+            Use one of the templates or create your own portfolio.
+          </h1>
+          <div className="flex flex-row items-start">
+            <span>
+              <a href="#" className="font-bold text-1xl text-black underline hover:text-gray-600">
+                Read more
+              </a>
             </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <Image src="/arrow.png" alt="Arrow" width={10} height={10} className="m-2" />
+          </div>
+        </div>
+      </section>
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      <section className="flex flex-col lg:flex-row bg-white pb-20 justify-center items-center">
+        <div className="flex flex-col lg:flex-col w-1/2 justify-center items-center">
+          <h1 className="flex text-black font-bold text-4xl lg:text-5xl" style={{ marginLeft: '150px' }}>
+            CREATING YOUR OWN PORTFOLIO
+          </h1>
+        </div>
+        <div className="flex flex-col lg:flex-col bg-white w-1/2 justify-end">
+          <div className="flex flex-row lg:flex-row">
+            <div className="flex flex-col lg:flex-col w-1/3 justify-center items-center">
+              <div className="circle text-white font-bold text-1xl lg:text-2xl" style={{ background: '#FFA500' }}>
+                1
+              </div>
+              <div className="circle_small" style={{ background: '#D3D3D3' }}></div>
+              <div className="circle_small" style={{ background: '#D3D3D3' }}></div>
+              <div className="circle_small" style={{ background: '#D3D3D3' }}></div>
+            </div>
+            <div className="flex w-2/3 m-4 justify-start">
+              <p className="text-black font-bold text-1xl lg:text-2xl mt-2">Choose the template</p>
+            </div>
+          </div>
+          <div className="flex flex-row lg:flex-row">
+            <div className="flex flex-col lg:flex-col w-1/3 justify-center items-center">
+              <div className="circle text-white font-bold text-1xl lg:text-2xl" style={{ background: '#FF8C00' }}>
+                2
+              </div>
+              <div className="circle_small" style={{ background: '#D3D3D3' }}></div>
+              <div className="circle_small" style={{ background: '#D3D3D3' }}></div>
+              <div className="circle_small" style={{ background: '#D3D3D3' }}></div>
+            </div>
+            <div className="flex w-2/3 m-4 justify-start">
+              <p className="text-black font-bold text-1xl lg:text-2xl mt-2">Customize</p>
+            </div>
+          </div>
+          <div className="flex flex-row lg:flex-row">
+            <div className="flex flex-col lg:flex-col w-1/3 justify-center items-center">
+              <div className="circle text-white font-bold text-1xl lg:text-2xl" style={{ background: '#FF6347' }}>
+                3
+              </div>
+            </div>
+            <div className="flex w-2/3 m-4 justify-start">
+              <p className="text-black font-bold text-1xl lg:text-2xl mt-2">Enjoy your portfolio</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section
+        className="flex flex-col bg-white shadow-lg w-full h-screen"
+        style={{
+          backgroundImage: "url('/MacBook.png')",
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          zIndex: 10,
+        }}
+      >
+        <div className="flex flex-col w-1/3 justify-start items-end p-4 ml-auto lg:flex-row">
+          <div className="text-left w-full" style={{ paddingTop: '20px', paddingLeft: '25px' }}>
+            <h1 className="text-black font-bold text-3xl mb-4 lg:text-6xl">WHY WE?</h1>
+            <p className="text-black font-medium text-sm mb-4 lg:text-xl mt-8 mb-4">
+              <span className="font-bold">01. </span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <p className="text-black font-medium text-sm mb-4 lg:text-xl mt-4 mb-4">
+              <span className="font-bold">02. </span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <p className="text-black font-medium text-sm mb-4 lg:text-xl mt-4 mb-8">
+              <span className="font-bold">03. </span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <button className="bg-black hover:bg-gray-800 text-white font-semibold py-4 px-6 lg:px-10 text-sm rounded-full flex shadow-md mt-4 lg:text-lg">
+              Start Creating
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col bg-white pt-20 justify-center items-center shadow-md" style={{ zIndex: 10 }}>
+        <h2 className="text-black font-bold text-3xl mb-4 lg:text-5xl">FAQs</h2>
+        <div className="flex flex-row mt-10 mb-20">
+          <div className="flex flex-col lg:w-1/3 pr-14 pl-14">
+            <p className="text-black font-medium text-1xl mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit?
+            </p>
+            <div className="flex flex-row items-start">
+              <span>
+                <a href="#" className="font-bold text-1xl text-black underline hover:text-gray-600">
+                  Get answer
+                </a>
+              </span>
+              <Image src="/arrow.png" alt="Arrow" width={10} height={10} className="m-2" />
+            </div>
+          </div>
+          <div className="flex flex-col lg:w-1/3 pr-14 pl-14">
+            <p className="text-black font-medium text-1xl mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit?
+            </p>
+            <div className="flex flex-row items-start">
+              <span>
+                <a href="#" className="font-bold text-1xl text-black underline hover:text-gray-600">
+                  Get answer
+                </a>
+              </span>
+              <Image src="/arrow.png" alt="Arrow" width={10} height={10} className="m-2" />
+            </div>
+          </div>
+          <div className="flex flex-col lg:w-1/3 pr-14 pl-14">
+            <p className="text-black font-medium text-1xl mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit?
+            </p>
+            <div className="flex flex-row items-start">
+              <span>
+                <a href="#" className="font-bold text-1xl text-black underline hover:text-gray-600">
+                  Get answer
+                </a>
+              </span>
+              <Image src="/arrow.png" alt="Arrow" width={10} height={10} className="m-2" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer className="p-10" style={{ backgroundImage: "url('/footer.png')" }}></footer>
+    </div>
   );
 }
