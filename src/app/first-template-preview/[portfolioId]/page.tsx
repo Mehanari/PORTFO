19eb4d88a963aa7f05edbe3b90d60ec3b96190da
Valeriia Shybika <91203182+sheiby13@users.noreply.github.com>
2024-05-18 12:21:29 +0300

    Designed right-side with user`s works

--- src/app/first-template-preview/[portfolioId]/page.tsx
+++ src/app/first-template-preview/[portfolioId]/page.tsx
@@ -59,52 +59,69 @@ export default function FirstTemplatePreview({ params }: { params: { portfolioId
   }
 
   return (
-    <div className="bg-rose-50 relative ">
-        <div className="gap-1 ml-20 ">
-            <div className="w-56 h-56 justify-center items-start inline-flex">
-                <img className="w-56 h-56 justify-center mt-16 items-center gap-2.5 relative rounded-full border border-zinc-600" src={protfolioData.photoPath} alt=""/>
-            </div>
-            <p className="w-56 grow shrink basis-0 text-center text-zinc-600 text-md font-normal mt-6">Username:</p>
-            <div className="w-56 pl-15 pr-15 py-1.5 mix-blend-darken bg-white rounded-3xl border border-zinc-500 justify-center items-center inline-flex mt-2">
-            <span className="w-20 h-5 text-center text-black text-sm font-normall">{protfolioData.username}</span>
-            </div>
-            <p className="w-56 grow shrink basis-0 text-center text-zinc-600 text-md font-normal mt-2">Full name:</p>
-            <div className="w-56 pl-15 pr-15 py-1.5 mix-blend-darken bg-white rounded-3xl border border-zinc-500 justify-center items-center inline-flex mt-2">
-                <span className="w-20 h-5 text-center text-black text-sm font-normal">{protfolioData.fullName}</span>
-            </div>
-            <p className="w-56 grow shrink basis-0 text-center text-zinc-600 text-md font-normal mt-2">Location:</p>
-            <div className="w-56 pl-15 pr-15 py-1.5 mix-blend-darken bg-white rounded-3xl border border-zinc-500 justify-center items-center inline-flex mt-2">
-                <span className="w-20 h-5 text-center text-black text-sm font-normal">{protfolioData.location}</span>
-            </div>
-            <p className="w-56 grow shrink basis-0 text-center text-zinc-600 text-md font-normal mt-2">Role:</p>
-            <div className="w-56 pl-15 pr-15 py-1.5 mix-blend-darken bg-white rounded-3xl border border-zinc-500 justify-center items-center inline-flex mt-2">
-                <span className="text-center text-black text-sm font-normal">{protfolioData.role}</span>
-            </div>
-            <p className="w-56 grow shrink basis-0 text-start text-zinc-600 text-md font-normal mt-2">Bio:</p>
-            <div className="w-56 pl-15 pr-15 py-1.5 mix-blend-darken bg-white rounded-3xl border border-zinc-500 justify-center items-center inline-flex">
-                <span className="text-center text-black text-sm font-normal">{protfolioData.bio}</span>
-            </div>
-            <p className="w-56 grow shrink basis-0 text-start text-zinc-600 text-md font-normal mt-2 mb-2">Links:</p>
-            {protfolioData.links.map((row, index) => (
-                <div className="text-black text-sm font-normal" key={index}>
-                    <a href={row} target="_blank" rel="noreferrer">{row}</a>
-                </div>
-            ))}
-        </div>
-        <p className="w-72 h-10 left-[846px] top-[100px] absolute text-center text-zinc-600 text-5xl font-bold">My works:</p><br/>
-        {protfolioData.projects.map((project, index) => (
-            <div key={index}>
-                <p>Project photo:</p>
-                <div className="w-40 aspect-video rounded flex items-center justify-center border-2 border-dashed cursor-pointer">
-                    <img src={project.photoPath} alt=""/>
-                </div>
-                <p>Project name:</p>
-                <p>{project.name}</p>
-                <p>Project link:</p>
-                <a href={project.link} target="_blank" rel="noreferrer">{project.link}</a>
-            </div>
-        ))}
-    </div>
+      <div className="bg-rose-50 relative font-sans ">
+          <div className="gap-1 ml-20 ">
+              <div className="w-56 h-56 justify-center items-start inline-flex">
+                  <img
+                      className="w-56 h-56 justify-center mt-16 items-center gap-2.5 relative rounded-full border border-zinc-600"
+                      src={protfolioData.photoPath} alt=""/>
+              </div>
+              <p className="w-56 grow shrink basis-0 text-center text-zinc-600 text-md mt-6 font-[Plus_Jakarta_Sans]">Username:</p>
+              <div
+                  className="w-56 pl-15 pr-15 py-1.5 mix-blend-darken max-h-screen bg-white rounded-3xl border border-zinc-500 justify-center items-center inline-flex mt-2">
+                  <span className="w-20 h-5 text-center text-black text-sm font-medium ">{protfolioData.username}</span>
+              </div>
+              <p className="w-56 grow shrink basis-0 text-center text-zinc-600 text-md font-normal mt-2">Full name:</p>
+              <div
+                  className="w-56 pl-15 pr-15 py-1.5 mix-blend-darken bg-white rounded-3xl border border-zinc-500 justify-center items-center inline-flex mt-2">
+                  <span className="w-20 h-5 text-center text-black text-sm font-medium">{protfolioData.fullName}</span>
+              </div>
+              <p className="w-56 grow shrink basis-0 text-center text-zinc-600 text-md font-normal mt-2">Location:</p>
+              <div
+                  className="w-56 pl-15 pr-15 py-1.5 mix-blend-darken bg-white rounded-3xl border border-zinc-500 justify-center items-center inline-flex mt-2">
+                  <span className="w-20 h-5 text-center text-black text-sm font-medium">{protfolioData.location}</span>
+              </div>
+              <p className="w-56 grow shrink basis-0 text-center text-zinc-600 text-md font-normal mt-2">Role:</p>
+              <div
+                  className="w-56 pl-15 pr-15 py-1.5 mix-blend-darken bg-white rounded-3xl border border-zinc-500 justify-center items-center inline-flex mt-2">
+                  <span className="text-center text-black text-sm font-medium">{protfolioData.role}</span>
+              </div>
+              <p className="w-56 grow shrink basis-0 text-start text-zinc-600 text-md font-normal mt-2">Bio:</p>
+              <div
+                  className="w-56 pl-15 pr-15 py-1.5 mix-blend-darken bg-white rounded-3xl border border-zinc-500 justify-center items-center inline-flex">
+                  <span className="text-center text-black text-sm font-medium">{protfolioData.bio}</span>
+              </div>
+              <p className="w-56 grow shrink basis-0 text-start text-zinc-600 text-md font-normal mt-2 mb-2">Links:</p>
+              {protfolioData.links.map((row, index) => (
+                  <div className="text-balance text-black text-sm font-normal" key={index}>
+                      <a href={row} target="_blank" rel="noreferrer">{row}</a>
+                  </div>
+              ))}
+          </div>
+          <div className="absolute left-[560px] top-[40px] shrink-0 mt-8 mr-7 max-w-full h-px border border-solid bg-stone-500 border-stone-500 w-[825px] max-md:mr-2.5"
+          ></div>
+          <p className=" w-72 h-10 left-[846px] top-[80px] absolute text-center text-zinc-600 text-5xl font-bold">My
+              works</p><br/>
+          <div className="absolute left-[560px] top-[104px]  flex justify-center items-center   gap-8 pb-8">
+              {protfolioData.projects.map((project, index) => (
+                  <div
+                      className="max-w-64 max-h-72 rounded-[50px] shadow border border-zinc-600 mt-16 flex flex-col justify-center items-center px-8 pt-6 pb-14 max-md:px-5 max-md:mt-8"
+                      key={index}>
+                      <p className="hidden">Project photo:</p>
+                      <div className=" relative w-32 items-center justify-center ">
+                          <img className="w-32 h-32 mt-4 flex justify-center rounded-full border border-zinc-600 self-center
+                    max-w-full aspect-square" src={project.photoPath} alt=""/>
+                      </div>
+                      <p className="hidden">Project name:</p>
+                      <p className="text-balance text-center text-black text-2xl font-medium mt-4 ">{project.name}</p>
+                      <p className="hidden">Project link:</p>
+                      <a className=" flex justify-center text-balance text-center text-black text-xs font-normal underline leading-tight mt-4 "
+                         href={project.link} target="_blank" rel="noreferrer">{project.link}</a>
+                  </div>
+              ))}
+          </div>
+
+      </div>
   )
 
 }
