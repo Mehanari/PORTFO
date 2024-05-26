d1ce94cc7aba1e561c3b0fd41f95a70759400ba5
Anastasiia <nastialime569@gmail.com>
2024-05-26 20:35:52 +0200

    Created layout for the first template

--- src/app/first-template-form/page.tsx
+++ src/app/first-template-form/page.tsx
@@ -6,6 +6,7 @@ import {savePortfolioDataForUser} from "@/functions/databaseAccess";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {auth} from "@/firebase/firebaseConfig";
 import {PortfolioStatus} from "@/portfolioStatuses";
+import Image from "next/image";
 
 type LinkRow = {
     id: number;
@@ -19,6 +20,9 @@ type ProjectRow = {
     photoPath: string;
 }
 
+// const getDefaultBackgroundColor = () => {
+//     return "bg-white";
+// };
 
 export default function FirstTemplateForm(){
     const [linksRows, setLinksRows] = useState<LinkRow[]>([]);
@@ -88,6 +92,32 @@ export default function FirstTemplateForm(){
         setProjects(projects.filter((project, i) => i !== index));
     };
 
+
+    // const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // const toggleMenu = () => {
+    //     setIsMenuOpen(!isMenuOpen);
+    // };
+
+    // const [backgroundColor, setBackgroundColor] = useState("bg-gradient-to-b from-yellow-100 to-blue-100");
+
+    // const handleColorChange = (color: string) => {
+    //     switch (color) {
+    //         case "orange":
+    //             setBackgroundColor("bg-orange-100");
+    //             break;
+    //         case "blue":
+    //             setBackgroundColor("bg-blue-100");
+    //             break;
+    //         case "green":
+    //             setBackgroundColor("bg-green-100");
+    //             break;
+    //         default:
+    //             setBackgroundColor(getDefaultBackgroundColor());
+    //             break;
+    //     }
+    // };
+
     if (loading) {
         return (
             <main className="flex justify-center items-center h-screen">
@@ -109,127 +139,175 @@ export default function FirstTemplateForm(){
     }
 
     return (
-        <div>
-            <label htmlFor="photo">Change photo:</label><br/>
-            <input
-                className="border-2"
-                type="file"
-                id="photo"
-                name="photo"
-                onChange={({target}) => {
-                    if (target.files) {
-                        const file = target.files[0];
-                        setPhotoPath(URL.createObjectURL(file));
-                        setPhoto(file);
-                    }
-                }}/><br/>
-            <div
-                className="w-40 aspect-video rounded flex items-center justify-center border-2 border-dashed cursor-pointer">
-                {photoPath ? (
-                    <img src={photoPath} alt=""/>
-                ) : (
-                    <span>Select Image</span>
-                )}
-            </div>
-            <label htmlFor="username">Username:</label><br/>
-            <input
-                className="border-2"
-                type="text"
-                id="username"
-                name="username"
-                value={username}
-                onChange={({target}) => setUsername(target.value)}/><br/>
-            <label htmlFor="fullname">Full name:</label><br/>
-            <input
-                className="border-2"
-                type="text"
-                id="fullname"
-                name="fullname"
-                value={fullname}
-                onChange={({target}) => setFullname(target.value)}/><br/>
-            <label htmlFor="location">Location:</label><br/>
-            <input
-                className="border-2"
-                type="text"
-                id="location"
-                name="location"
-                value={location}
-                onChange={({target}) => setLocation(target.value)}/><br/>
-            <label htmlFor="role">Role:</label><br/>
-            <input
-                className="border-2"
-                type="text"
-                id="role"
-                name="role"
-                value={role}
-                onChange={({target}) => setRole(target.value)}/><br/>
-            <label htmlFor="bio">Bio:</label><br/>
-            <textarea
-                className="border-2"
-                id="bio"
-                name="bio"
-                value={bio}
-                onChange={({target}) => setBio(target.value)}></textarea><br/>
-            <label htmlFor="links">Links:</label><br/>
-            {linksRows.map((row) => (
-                <div key={row.id}>
-                    <input
-                        className="border-2"
-                        type="url"
-                        id="links"
-                        name="links"
-                        value={linksRows[row.id].link}
-                        onChange={({target}) => onLinkChange(row.id, target.value)}
-                    /><br/>
-                    <button onClick={() => handleDeleteLink(row.id)}>Delete</button>
-                    <br/>
+        // <div className={`font-cursive ${backgroundColor}`}>
+        <div className="font-cursive bg-white">
+            {/* {isMenuOpen && ( 
+                <div className="flex flex-row w-full justify-end pr-20 pt-5">}
+                    <button className="bg-orange-200 hover:bg-orange-300 text-gray font-semibold py-2 px-4 rounded-md mr-2 border-2 border-gray-500" onClick={() => handleColorChange("orange")}>Orange</button>
+                    <button className="bg-blue-200 hover:bg-blue-300 text-gray font-semibold py-2 px-4 rounded-md mr-2 border-2 border-gray-500" onClick={() => handleColorChange("blue")}>Blue</button>
+                    <button className="bg-green-200 hover:bg-green-300 text-gray font-semibold py-2 px-4 rounded-md mr-2 border-2 border-gray-500" onClick={() => handleColorChange("green")}>Green</button>
+                    <button className="bg-white hover:bg-gray-100 text-gray font-semibold py-2 px-4 rounded-md border-2 border-gray-500" onClick={() => handleColorChange("default")}>Default</button>
+                </div>)} */}
+            <div className="flex flex-row pl-20 pr-20">
+                <div className="flex justify-end items-center w-2/3 h-20 ml-auto border-b-2 border-gray-500">
+                    <button className="mr-4"> {/*TODO send to the previous page*/}
+                        <Image src="/ArrowPrev.png" alt="" width={54} height={54} className="mr-2" />
+                    </button>
+                    <button className="mr-4"> {/*onClick={toggleMenu}}*/} {/*TODO change template color*/}
+                        <Image src="/ColorPalette.png" alt="" width={54} height={54} className="mr-2" />
+                    </button>
+                    <button className="mr-4"> {/*TODO show preview*/}
+                        <Image src="/PreviewButton.png" alt="" width={54} height={54} className="mr-2" />
+                    </button>
+                    <button
+                        onClick={handleSave}> {/*TODO save changes*/}
+                        <Image src="/SaveButton.png" alt="" width={54} height={54} className="mr-2" />
+                    </button>
                 </div>
-            ))}
-            <button onClick={handleAddLink}>Add Link</button>
-            <br/>
-            <label htmlFor="projects">My works:</label><br/>
-            {projects.map((project) => (
-                <div key={project.id}>
-                    <label htmlFor="project-photo">Project photo:</label><br/>
+            </div>
+            
+            <div className="flex flex-row">
+                <div className="flex flex-col items-center justify-start w-1/3 pl-8 pr-8">
+                    <label htmlFor="photo"></label> 
                     <input
-                        className="border-2"
+                        className="hidden"
                         type="file"
-                        id="project-photo"
-                        name="project-photo"
+                        id="photo"
+                        name="photo"
                         onChange={({target}) => {
                             if (target.files) {
                                 const file = target.files[0];
-                                handleEditProjectPhoto(project.id, file, URL.createObjectURL(file));
+                                setPhotoPath(URL.createObjectURL(file));
+                                setPhoto(file);
                             }
                         }}/><br/>
                     <div
-                        className="w-40 aspect-video rounded flex items-center justify-center border-2 border-dashed cursor-pointer">
-                        { project.photoPath ? (
-                            <img src={project.photoPath} alt=""/>
+                        className="w-60 h-60 bg-white rounded-full flex items-center justify-center border-2 cursor-pointer border-gray-500"
+                        onClick={() => document.getElementById('photo')?.click()}>
+                        {photoPath ? (
+                            <img src={photoPath} alt="" className="w-full h-full rounded-full object-cover"/>
                         ) : (
-                            <span>Select Image</span>
+                            <span><img src="/Vector.png" alt="" style={{height: "30px"}}/></span>
                         )}
                     </div>
-                    <label htmlFor="project-name">Project name:</label><br/>
+                    <label htmlFor="username" className="pt-8 font-semibold">Username</label>
                     <input
-                        className="border-2"
+                        className="border-2 rounded-2xl w-1/2 border-gray-500"
                         type="text"
-                        id="project-name"
-                        name="project-name"
-                        value={project.name}
-                        onChange={({target}) => handleEditProjectName(project.id, target.value)}/><br/>
-                    <label htmlFor="project-link">Project link:</label><br/>
+                        id="username"
+                        name="username"
+                        value={username}
+                        onChange={({target}) => setUsername(target.value)}/>
+                    <label htmlFor="fullname" className="pt-2 font-semibold">Full name</label>
                     <input
-                        className="border-2"
+                        className="border-2 rounded-2xl w-1/2 border-gray-500"
                         type="text"
-                        id="project-link"
-                        name="project-link"
-                        value={project.link}
-                        onChange={({target}) => handleEditProjectLink(project.id, target.value)}/><br/>
-                    <button onClick={() => handleDeleteProject(project.id)}>Delete</button>
+                        id="fullname"
+                        name="fullname"
+                        value={fullname}
+                        onChange={({target}) => setFullname(target.value)}/>
+                    <label htmlFor="location" className="pt-2 font-semibold">Location</label>
+                    <input
+                        className="border-2 rounded-2xl w-1/2 border-gray-500"
+                        type="text"
+                        id="location"
+                        name="location"
+                        value={location}
+                        onChange={({target}) => setLocation(target.value)}/>
+                    <label htmlFor="role" className="pt-2 font-semibold">Role</label>
+                    <input
+                        className="border-2 rounded-2xl w-1/2 border-gray-500"
+                        type="text"
+                        id="role"
+                        name="role"
+                        value={role}
+                        onChange={({target}) => setRole(target.value)}/>
+
+
+                    <div className="flex flex-col w-1/2">
+                        <label htmlFor="bio" className="pt-8 font-semibold">Bio</label>
+                        <textarea
+                            className="border-2 rounded-2xl w-full border-gray-500"
+                            id="bio"
+                            name="bio"
+                            value={bio}
+                            onChange={({ target }) => setBio(target.value)}></textarea>
+                    </div>
+
+                    <label htmlFor="links" className="pt-6 pb-2 font-semibold">Links</label>
+                    {linksRows.map((row) => (
+                        <div key={row.id}>
+                            <div className="flex flex-col justify-center items-center">
+                                <input
+                                    className="border-2 rounded-2xl w-full border-gray-500"
+                                    type="url"
+                                    id="links"
+                                    name="links"
+                                    value={linksRows[row.id].link}
+                                    onChange={({target}) => onLinkChange(row.id, target.value)}
+                                /><br/>
+                                <button onClick={() => handleDeleteLink(row.id)} className="text-gray font-semibold underline">Delete</button>
+                                <br/>
+                            </div>
+                        </div>
+                    ))}
+                    <button onClick={handleAddLink} className="bg-gray-200 hover:bg-gray-300 text-gray font-semibold py-2 px-6 rounded-full flex items-center">Add Link</button>
+                    <br/>
                 </div>
-            ))}
-            <button onClick={handleAddProject}>Add Project</button><br/>
+
+
+                <div className="flex flex-col w-2/3 pr-20 pt-20 items-center">
+                    <label htmlFor="projects" className="text-5xl">
+                        MY WORKS
+                    </label><br/>
+                    <div className="flex flex-row flex-wrap justify-center">
+                        {projects.map((project) => (
+                            <div key={project.id} className="flex flex-col justify-center items-center border-2 border-gray-500 rounded-3xl m-5 pl-32 pr-32 pt-4 pb-4">
+                                <label htmlFor="project-photo"></label>
+                                <input
+                                    className="hidden"
+                                    type="file"
+                                    id="project-photo"
+                                    name="project-photo"
+                                    onChange={({target}) => {
+                                        if (target.files) {
+                                            const file = target.files[0];
+                                            handleEditProjectPhoto(project.id, file, URL.createObjectURL(file));
+                                        }
+                                    }}/>
+                                <div
+                                    className="w-40 h-40 bg-white rounded-full flex items-center justify-center border-2 cursor-pointer border-gray-500"
+                                    onClick={() => document.getElementById('project-photo')?.click()}>
+                                    { project.photoPath ? (
+                                        <img src={project.photoPath} alt="" className="w-full h-full rounded-full object-cover"/>
+                                    ) : (
+                                        <span><img src="/Vector.png" alt="" style={{height: "30px"}}/></span>
+                                    )}
+                                </div>
+                                <label htmlFor="project-name" className="pt-2 font-semibold">Project name</label>
+                                <input
+                                    className="border-2 rounded-2xl w-full border-gray-500"
+                                    type="text"
+                                    id="project-name"
+                                    name="project-name"
+                                    value={project.name}
+                                    onChange={({target}) => handleEditProjectName(project.id, target.value)}/>
+                                <label htmlFor="project-link" className="pt-2 font-semibold">Project link</label>
+                                <input
+                                    className="border-2 rounded-2xl w-full border-gray-500"
+                                    type="text"
+                                    id="project-link"
+                                    name="project-link"
+                                    value={project.link}
+                                    onChange={({target}) => handleEditProjectLink(project.id, target.value)}/>
+                                <button onClick={() => handleDeleteProject(project.id)} className="bg-gray-200 hover:bg-gray-300 text-gray font-semibold py-2 px-6 rounded-full flex items-center mt-4">Delete</button>
+                            </div>
+                        ))}
+                    </div>
+                    
+                    <button onClick={handleAddProject} className="bg-gray-200 hover:bg-gray-300 text-gray font-semibold py-2 px-6 rounded-full flex items-center">Add Project</button><br/>
+                </div>
+            </div>
             <button onClick={handleSave}>Save</button>
         </div>
     );
