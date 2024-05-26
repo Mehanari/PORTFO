c4402cf0bd320d1debfece835cd88f27992a40a6
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-05-26 15:08:27 +0300

    Made validation for the first template and update of the saved portfolio.

--- src/functions/databaseAccess.ts
+++ src/functions/databaseAccess.ts
@@ -2,7 +2,7 @@ import {PortfolioData as SecondTemplateData, ProjectData as SecondTemplateProjec
 import {PortfolioData as FirstTemplateData, ProjectData as FirstTemplateProjectData} from "@/model/firstTemplateTypes";
 import {getStorage, ref, uploadBytes} from "@firebase/storage";
 import {db} from "@/firebase/firebaseConfig";
-import {addDoc, collection} from "@firebase/firestore";
+import {addDoc, collection, doc, updateDoc} from "@firebase/firestore";
 import {getFileHash} from "@/functions/cryptographyUtilities";
 import {
     IMAGES_DIRECTORY_NAME,
@@ -49,6 +49,51 @@ async function saveFirstTemplateDataForUser(userId: string, data: FirstTemplateD
                 projects: firebaseProjects,
                 bio: data.bio,
             });
+        return docRef.id;
+    } catch (error) {
+        console.error('Error saving template data for user with id: ' + userId + '\nError: ' + error);
+        return;
+    }
+}
+
+export async function updateFirstTemplateDataForUser(userId: string, data: FirstTemplateData, portfolioId: string): Promise<string | undefined> {
+    try {
+        type FirebaseProjectData = {
+            filePath: string;
+            name: string;
+            link: string;
+        }
+        let photoPath: string = "";
+        if (data.photo) {
+            photoPath = await addImage(data.photo);
+        }
+        let firebaseProjects: FirebaseProjectData[] = [];
+        for (const project of data.projects) {
+            let projectPhotoPath: string = "";
+            if (project.photo) {
+                projectPhotoPath = await addImage(project.photo);
+            }
+            firebaseProjects.push({
+                filePath: projectPhotoPath,
+                name: project.name,
+                link: project.link,
+            });
+        }
+        const docRef = doc(collection(db, PORTFOLIOS_COLLECTION_NAME), portfolioId);
+        await updateDoc(docRef,{
+            templateType: TemplateType.FIRST_TEMPLATE,
+            userId: userId,
+            name: data.name,
+            status: data.status,
+            link: data.link,
+            photoPath: photoPath,
+            username: data.username,
+            fullName: data.fullName,
+            location: data.location,
+            role: data.role,
+            projects: firebaseProjects,
+            bio: data.bio,
+        });
         return docRef.id;
     } catch (error) {
         console.error('Error saving template data for user with id: ' + userId + '\nError: ' + error);
